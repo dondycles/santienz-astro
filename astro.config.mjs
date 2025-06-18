c87806697108d7950cc3c312f1ea869c0df7bf46
dondycles <johnrod.dondoyano@gmail.com>
2025-06-19 01:29:29 +0800

    with adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react()],
+  adapter: vercel(),
 });

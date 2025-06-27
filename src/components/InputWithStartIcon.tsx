3f38486bc5c3eb983cc97a68e065b79ec077f849
dondycles <johnrod.dondoyano@gmail.com>
2025-06-27 15:00:45 +0800

    text-foreground

--- src/components/InputWithStartIcon.tsx
+++ src/components/InputWithStartIcon.tsx
@@ -16,7 +16,14 @@ export default function InputWithStartIcon({
 }) {
   return (
     <div className="relative">
-      <Input id={id} className={cn("peer ps-9", className)} name={name} />
+      <Input
+        id={id}
+        className={cn(
+          "peer ps-9 text-foreground placeholder:text-foreground",
+          className,
+        )}
+        name={name}
+      />
       <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
         <Search size={16} aria-hidden="true" />
       </div>

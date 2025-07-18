fff4378d3dbee6381a8c78ba6218badc1da84232
dondycles <johnrod.dondoyano@gmail.com>
2025-07-19 01:00:26 +0800

    major edits

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,25 +2,28 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 export async function imageSrcsToDataURIs(srcs: string[]): Promise<string[]> {
-	return Promise.all(
-		srcs.map((src) =>
-			fetch(src)
-				.then((res) => res.blob())
-				.then(
-					(blob) =>
-						new Promise<string>((resolve, reject) => {
-							const reader = new FileReader();
-							reader.onloadend = () => {
-								if (typeof reader.result === "string") resolve(reader.result);
-								else reject(new Error("Failed to read as data URI"));
-							};
-							reader.onerror = reject;
-							reader.readAsDataURL(blob);
-						}),
-				),
-		),
-	);
+  return Promise.all(
+    srcs.map((src) =>
+      fetch(src)
+        .then((res) => res.blob())
+        .then(
+          (blob) =>
+            new Promise<string>((resolve, reject) => {
+              const reader = new FileReader();
+              reader.onloadend = () => {
+                if (typeof reader.result === "string") resolve(reader.result);
+                else reject(new Error("Failed to read as data URI"));
+              };
+              reader.onerror = reject;
+              reader.readAsDataURL(blob);
+            }),
+        ),
+    ),
+  );
 }
+
+export const imgUrlFromSb = (bucket: string, path: string) =>
+  `${import.meta.env.SUPABASE_URL}/storage/v1/object/public/${bucket}/${path}`;

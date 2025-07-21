c195b82ba3107dd2c81fa427fa08013f567e9f21
dondycles <johnrod.dondoyano@gmail.com>
2025-07-21 14:15:04 +0800

    products page

--- src/lib/creds-info.ts
+++ src/lib/creds-info.ts
@@ -4,7 +4,7 @@ import coc from "@/images/certs/coc-2025.jpg";
 import epo from "@/images/certs/epo-2025.jpg";
 import fsic from "@/images/certs/fsic-2025.jpg";
 import mp from "@/images/certs/mp-2025.jpg";
-import or from "@/images/certs/or-2025.jpg";
+// import or from "@/images/certs/or-2025.jpg";
 
 export const CERTS_INFO: {
   title: string;
@@ -21,5 +21,5 @@ export const CERTS_INFO: {
   { title: "Business Permit", img: bp, type: "permit" },
   { title: "Environmental Permit to Operate", img: epo, type: "permit" },
   { title: "Mayor's Permit", img: mp, type: "permit" },
-  { title: "Official Receipt", img: or, type: "receipt" },
+  // { title: "Official Receipt", img: or, type: "receipt" },
 ];

81a9fcf5b8781d84b114217aba2d21307dafe4a8
dondycles <johnrod.dondoyano@gmail.com>
2025-07-21 17:24:56 +0800

    html fix

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -41,7 +41,46 @@ export const server = {
           subject: input.subject,
           replyTo: input.email,
           text: input.body,
-          html: `...`, // your HTML stays the same
+          html: `<html>
+  <head>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div
+      style="display: flex; align-items: center; background-color: #182e85; padding: 16px;"
+    >
+      <img
+        src="https://santienz-astro.vercel.app/santienz.png"
+        style="height: 32px;"
+      />
+      <h2 style="color: white; margin-left:16px; ">
+        Santienz Phils. Inc.
+      </h2>
+    </div>
+    <div style="padding: 0 16px;">
+      <h3 style="color:#182e85; font-size: 24px; margin: 24px 0;">
+        Hi, there is a new inquiry from website.
+      </h3>
+      <div
+        style="background-color: #182e8520; border-radius: 16px; padding: 16px;"
+      >
+        <h4>From: ${input.name} ${input.email}</h4>
+        <h4>Subject: ${input.subject}</h4>
+        <br />
+        <p style="white-space: pre-wrap;">${input.body}</p>
+      </div>
+
+      <br />
+      <br />
+      <p>Reply to: ${input.email}</p>
+    </div>
+  </body>
+</html>`, // your HTML stays the same
         });
 
         if (email.error) {

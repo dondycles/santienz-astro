44e4485c2f49df7b97084d28820c561e1a1b907c
dondycles <johnrod.dondoyano@gmail.com>
2025-07-24 22:25:32 +0800

    vercel fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
